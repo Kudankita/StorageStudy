3e3a4fc7f2e9c7aaacf779a7f190028384579b08
Silver <41056212+SilverCafe@users.noreply.github.com>
2019-06-10 21:11:14 +0900

    完了

--- StorageStudy/MainPage.xaml.cs
+++ StorageStudy/MainPage.xaml.cs
@@ -1,30 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
-
-namespace StorageStudy
+﻿namespace StorageStudy
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using Windows.Storage;
+    using Windows.Storage.Search;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The Start_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+            var txtFile = await KnownFolders.DocumentsLibrary.GetFileAsync("Test.txt"); //これもCドライブしかわからないようだった
+            Debug.WriteLine(txtFile.DisplayName);
+            Debug.WriteLine(txtFile.Path);
+
+            //https://docs.microsoft.com/ja-jp/windows/uwp/files/quickstart-reading-and-writing-files より
+            var buffer = await FileIO.ReadBufferAsync(txtFile);
+            using (var dataReader = Windows.Storage.Streams.DataReader.FromBuffer(buffer))
+            {
+                string text = dataReader.ReadString(buffer.Length);
+                Debug.WriteLine(text);
+            }
+
+
+
+            //var testFile = await KnownFolders.PicturesLibrary.GetFileAsync("Test.txt");
+            QueryOptions queryOption = new QueryOptions
+        (CommonFileQuery.OrderByTitle, new string[] { ".mp3", ".mp4", ".wma" }); //見つからなかった。Cドライブ限定のようだ
+
+
+            queryOption.FolderDepth = FolderDepth.Deep;
+
+            Queue<IStorageFolder> folders = new Queue<IStorageFolder>();
+
+            var files = await KnownFolders.PicturesLibrary.CreateFileQueryWithOptions
+              (queryOption).GetFilesAsync();
+
+            foreach (var file in files)
+            {
+                Debug.WriteLine(file.DisplayName);
+            }
+
+
         }
     }
 }
